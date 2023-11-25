a9facb96b6b9d76e3d617a3ff7cf852026c2903f
Carlos Eduardo <caeca425@gmail.com>
2023-11-24 20:57:27 -0500

    se crea portafolio con imágenes y estilos

--- src/componentes/Portafolio.js
+++ src/componentes/Portafolio.js
@@ -7,11 +7,15 @@ export const Portafolio = () => {
     <div className='page'>
       <h1 className='heading'>Portafolio</h1>
 
+      <section className='works'>
       {
         trabajos.map(trabajo => ( //Este paréntisis indica que se maneja un return automático, pero también podemos declarar un return y funciona igual. POr lo general lleva corchetes pero estos no funcionan como return
 
           //console.log(trabajo.nombre);
-          <article key={trabajo.id}>
+          <article key={trabajo.id} className='work-item'>
+            <div className='mask'>
+              <img src={'/images/' + trabajo.id + '.png'}></img>
+            </div>
             <span>{trabajo.categorias}</span>
             <h2>{trabajo.nombre}</h2>
             <h3>{trabajo.tecnologias}</h3>
@@ -19,6 +23,7 @@ export const Portafolio = () => {
           </article>
         ))
       }
+      </section>
 
     </div>
   )
